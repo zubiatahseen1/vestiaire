dd6324258ad7d4b9bae61db686a6aff84174d1e7
Zubia Tahseen <zubia.tahseen@triosstudent.com>
2026-05-29 10:37:51 -0400

    Initial Commit

---
+++ vestiaire/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vestiaire
+//
+//  Created by Zubia Tahseen on 2026-05-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vestiaire/vestiaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  vestiaireApp.swift
+//  vestiaire
+//
+//  Created by Zubia Tahseen on 2026-05-29.
+//
+
+import SwiftUI
+
+@main
+struct vestiaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
